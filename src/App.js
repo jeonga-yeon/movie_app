3b9cab4159a7ce95270b292725fd8a413a9a653c
joyfuldev <wings2132@naver.com>
2021-05-05 00:53:12 +0900

    Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React from 'react';
 
-function Food({fav}) {
-  return <h1>I like {fav}</h1>;
+function Food({name, picture}) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>  
+  )
 }
+
+const foodILike = [
+  {
+    name: "만두",
+    image: "https://images.unsplash.com/photo-1619926096619-5956ab4dfb1b?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDN8eGpQUjRobGtCR0F8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    name: "cake",
+    image: "https://images.unsplash.com/photo-1620026801439-9bbc327b29f2?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDR8eGpQUjRobGtCR0F8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    name: "coffe",
+    image: "https://unsplash.com/photos/DqrqLkM1CMU"
+  },
+  {
+    name: "마카롱",
+    image: "https://images.unsplash.com/photo-1616969083845-bba6ddf12be2?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM3fHhqUFI0aGxrQkdBfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+  }
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food fav="pizza" />
-      <Food fav="pasta" />
-      <Food fav="potato" />
-      <Food fav="egg" />
+      {foodILike.map(dish => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   )
 }
