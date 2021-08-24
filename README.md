e54693035e38fbd5ede95534b6d94563f12fa86d
joyfuldev <wings2132@naver.com>
2021-08-24 22:31:10 +0900

    readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Movie App
 
 React JS 공부
+
+# 결과물
+최신 영화 정보 소개
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,17 +3,18 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ year, title, summary, poster, genres }) {
+function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <Link to={{
-      pathname: "/movie-detail",
-      state: {
-        year,
-        title,
-        summary,
-        poster,
-        genres
-      }
+    <Link 
+      to={{
+        pathname: `/movie/${id}`,
+        state: {
+          year,
+          title,
+          summary,
+          poster,
+          genres
+        }
      }}
     >
       <div className="movie">
